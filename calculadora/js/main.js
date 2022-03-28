70e68aa19664e5c04ca9c559e4e5bb4fb89c4cec
facundoangel <facundonicolasangel@gmail.com>
2022-03-27 23:51:43 -0300

    terminado de maquetacion calculadora

---
+++ calculadora/js/main.js
@@ -0,0 +1,93 @@
+//FUNCTIONS=====================================================================
+const writeNumber = (digits) =>{
+
+    let digitsInScreen = $(".digits").text();
+
+    if(digitsInScreen=="0"){
+        
+        if(digits!="0") $(".digits").text(digits);
+
+    } else if(digits == "0" ){
+        
+        /*if(!digitsInScreen.match(/[-+/x] $/g)){
+            $(".digits").text(digitsInScreen + digits)
+        }*/
+        $(".digits").text(digitsInScreen + digits)
+    }
+    else{
+        
+        if(digitsInScreen.length < 70){
+            $(".digits").text(digitsInScreen + digits)
+        }
+
+    }
+    
+}
+
+const WriteDot = (digit) =>{
+    let digitsInScreen = $(".digits").text();
+
+ 
+
+    //VALIDATIONS FOR THE USE OF THE DOT
+    if(!digitsInScreen.match(/[ ,+-/x$]/g) && !digitsInScreen.includes(" ")){
+        $(".digits").text(digitsInScreen + digit);
+        
+
+    } else if(digitsInScreen.includes(" ") && digitsInScreen.match(/[0-9]$/g)){
+
+        if(digitsInScreen.match(/,/g) == null){
+            $(".digits").text(digitsInScreen + digit);
+            
+            
+        } else if((digitsInScreen.match(/,/g).length < 2) && (digitsInScreen.split(",")[1].includes(" "))) {
+            $(".digits").text(digitsInScreen + digit);
+            
+        }
+    } 
+
+    
+}
+
+const WriteOperation = (digit) =>{
+    let digitsInScreen = $(".digits").text();
+    re = /[-*+/]/g;
+
+    if(digitsInScreen!=="0" && !digitsInScreen.match(re) && digitsInScreen[digitsInScreen.length-1] !== "."){
+        
+        $(".digits").text(digitsInScreen + ' ' + digit + " ");
+
+    }
+}
+
+const delOperation = ()=>{
+    let textDisplay = $(".digits").text();
+    let lenghtText = textDisplay.length;
+    let lastChar = textDisplay[lenghtText-1];
+
+    if(lastChar===" ") $(".digits").text(textDisplay.slice(0,lenghtText-3))
+    else if(lenghtText > 1) $(".digits").text(textDisplay.slice(0,lenghtText-1))
+    else $(".digits").text("0");
+}
+
+
+//HANDLE CLICKS==============================================================
+$(".key-number").on("click",function(){
+    writeNumber($(this).text());
+})
+
+$("#key-dot").on("click",function(){
+    WriteDot(",");
+})
+
+$("#key-reset").on("click",function(){
+    $(".digits").text("0");
+})
+
+$("#key-del").on("click",function(){
+    delOperation();
+})
+
+$(".key-operation").on("click",function(){
+    WriteOperation($(this).text());
+})
